d8829b124c971a8facf551e9170c6def14eef58a
Akiko Green <agreen@alphaworks.tech>
2022-11-22 13:51:33 -0800

    feat(Employee): http  error handling

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,23 +1,26 @@
 import { IEmployee } from './employee';
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { ErrorHandler, Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import{ catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({ //required for services and not componets
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  private _url: string = "/assets/data/employees.json"
+  private _url: string = "/assets/data/employees1.json"
 
   constructor( private http: HttpClient) { }
-
-
   
-// provide employee data
   getEmployees(): Observable<IEmployee[]>{
-
-    return this.http.get<IEmployee[]>(this._url);
+    return this.http.get<IEmployee[]>(this._url).pipe(catchError(this.errorHandler));
 
   }
+
+  errorHandler(error: HttpErrorResponse){
+    return throwError(error.message || "Server Error");
+  }
+
 }
